b4942dd9f408b8217217b1ec17e0ddf073c22043
ambar13 <32307145+ambar13@users.noreply.github.com>
2017-10-09 12:17:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function cifradoCesar(frase, n){
+    var alfa = 'ABCDEFGHIJKLMOPQRSTUVXYZ';
+    alfa.charCodeAt(n);
+  fraseCifrada = frase.split('');
+  for (var i=0; i<frase.length; i++){
+  for (var j=0; j<n; j++){
+    if((n - 65 + 33) % 26 + 65){
+        return 
+  }
+  else if(fraseCifrada[i]==90) 
+  fraseCifrada[i]='A'; 
+else if(fraseCifrada[i]==122) 
+  fraseCifrada[i]='a'; 
+} 
+} 
+return frase;
+}
+
+
+
